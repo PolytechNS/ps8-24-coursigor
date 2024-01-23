f2722a91a5981ace1131eb12cb68c9cc5342bf4d
Swannben <swannbenz@gmail.com>
2024-01-23 14:22:39 +0100

    test dans les sockets

--- back/index.js
+++ back/index.js
@@ -1,5 +1,6 @@
 // The http module contains methods to handle http queries.
 const http = require('http')
+const { Server } = require("socket.io");
 // Let's import our logic.
 const fileQuery = require('./queryManagers/front.js')
 const apiQuery = require('./queryManagers/api.js')
@@ -7,7 +8,8 @@ const apiQuery = require('./queryManagers/api.js')
 /* The http module contains a createServer function, which takes one argument, which is the function that
 ** will be called whenever a new request arrives to the server.
  */
-http.createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
+    console.log("server")
     // First, let's check the URL to see if it's a REST request or a file request.
     // We will remove all cases of "../" in the url for security purposes.
     let filePath = request.url.split("/").filter(function(elem) {
@@ -27,5 +29,19 @@ http.createServer(function (request, response) {
         response.statusCode = 400;
         response.end(`Something in your request (${request.url}) is strange...`);
     }
+    console.log(`Request for ${request.url} received.`);
 // For the server to be listening to request, it needs a port, which is set thanks to the listen function.
-}).listen(8000);
+})
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log("user disconnected")
+    })
+  });
+
+server.listen(8000);
+
+
