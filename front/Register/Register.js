4d9a191688e9f39a86ee6f8094ec7a907282669f
Lorenzo Froment <lorenzo.froment@etu.unice.fr>
2024-02-06 17:45:54 +0100

    push to have docker

--- front/Register/Register.js
+++ front/Register/Register.js
@@ -1,28 +1,35 @@
-document.getElementById('loginBtn').addEventListener('click', function() {
-    var username = document.getElementsByName('username').value;
-    var password = document.getElementsByName('password').value;
-    var data = {
-        username: username,
-        password: password
-    };
-    fetch('http://localhost:8765/login', {
+document.getElementById('RegisterBtn').addEventListener('click', function() {
+    console.log('Bouton de connexion clique.');
+
+
+    // Get input values
+    const email = document.getElementsByName('email')[0].value;
+    const username = document.getElementsByName('username')[0].value;
+    const password = document.getElementsByName('password')[0].value;
+
+    const postData = {"username": username, "password": password, "email": email}
+
+    // Send POST request
+    fetch('http://localhost:8765/Register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body:JSON.stringify(postData)
     })
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-            return response.json();
-        })
-        .then(data2 => {
-            console.log('Données de la réponse :', data2);
-            // Handle the response data as needed
-        })
-        .catch(error => {
-            console.error('Erreur lors de la requête :', error);
+            const contentType = response.headers.get('content-type');
+            if (contentType && contentType.includes('application/json')) {
+                return response.json();
+            } else {
+                // Affichez le contenu de la réponse pour déboguer
+                return response.text().then(text => {
+                    console.log('Non-JSON response:', text);
+                    throw new Error('Response is not in JSON format');
+                });
+            }
         });
 });
