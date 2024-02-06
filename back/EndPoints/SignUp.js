a8c38500a03ffefd6cfbbaa8c303f609a95dce4f
Swannben <swannbenz@gmail.com>
2024-02-06 17:58:54 +0100

    changed signup.js so it would connect to the database

--- back/EndPoints/SignUp.js
+++ back/EndPoints/SignUp.js
@@ -1,3 +1,9 @@
-const signUp = async (mail, username, password) => {
+const { MongoClient } = require("mongodb")
 
-}
+const DBuri = "mongodb://root:example@mongodb:27017/";
+const DBClient = new mongo.MongoClient(DBuri);
+
+const signUp = async (mail, username, password) => {
+    MongoClient.connect(DBuri, async (err, client) => {
+        
+})}
