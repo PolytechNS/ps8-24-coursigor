6156ba2f01a523dad8b2824df850ade7b725d70d
Swannben <swannbenz@gmail.com>
2024-02-13 14:49:01 +0100

    skeleton of the AI

--- back/queryManagers/G.js
+++ back/queryManagers/G.js
@@ -1,33 +1,65 @@
-const gameState = {
-  opponentWalls: [],
-  ownWalls: [],
-  board: [
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-  ],
-  constructor(opponentWalls, ownWalls, board) {
-    this.opponentWalls = opponentWalls;
-    this.ownWalls = ownWalls;
-    this.board = board;
-  }
-};
+import { GameState, Move } from './Classes';
 
-class Move {
-  constructor(action, value) {
-    this.action = action;
-    this.value = value;
-  }
+var whichPlayer = 0;
+
+function setup(AIplay) {
+    return new Promise((resolve, reject) => {   
+        if (AIplay == 1){
+            whichPlayer = 1;
+            resolve( "[4,0]");
+        }
+        else {
+            
+            resolve("[4,8]");
+        }
+        setTimeout(() => {
+            reject('timeout');
+        }, 1000);
+    });
 }
 
+function nextMove(gameState) {
+    return new Promise ((resolve, reject) => {
+    for (var i=0; i<9; i++){
+        for (j=0; j<9; j++){
+            resolve(whatToDo(whichPlayer, gameState));
+        }
+    }
+    setTimeout(() => {
+        reject('timeout');
+    }, 200);
+    });
+}
 
-// Example usage:
-console.log(gameState.opponentWalls);
-console.log(gameState.ownWalls);
-console.log(gameState.board);
+function correction(rightMove) {
+    return new Promise((resolve, reject) => {
+        resolve(true);
+        setTimeout(() => {reject('timeout');}, 50);
+    });
+}
+
+function updateBoard(gameState) {
+    return new Promise((resolve,reject)=>{ 
+        resolve(true);
+        setTimeout(() => {reject('timeout');}, 50);
+    });
+}   
+
+
+function whatToDo(whichPlayer, gameState){
+    let advance;
+    if (whichPlayer == 1){
+        advance = 1;
+    }
+    else {
+        advance = -1;
+    }
+    if (gameState.board[i][j] == 1){
+        return new Move("move", [i+advance, j]);
+    }
+}
+
+exports.setup = setup;
+exports.nextMove = nextMove;    
+exports.correction = correction;    
+exports.updateBoard = updateBoard;
