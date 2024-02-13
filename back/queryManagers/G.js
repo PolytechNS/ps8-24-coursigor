2a96d39143499ca7deae98b07efec56be57de086
Swannben <swannbenz@gmail.com>
2024-02-13 13:36:15 +0100

    created the classes

---
+++ back/queryManagers/G.js
@@ -0,0 +1,33 @@
+const gameState = {
+  opponentWalls: [],
+  ownWalls: [],
+  board: [
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+  ],
+  constructor(opponentWalls, ownWalls, board) {
+    this.opponentWalls = opponentWalls;
+    this.ownWalls = ownWalls;
+    this.board = board;
+  }
+};
+
+class Move {
+  constructor(action, value) {
+    this.action = action;
+    this.value = value;
+  }
+}
+
+
+// Example usage:
+console.log(gameState.opponentWalls);
+console.log(gameState.ownWalls);
+console.log(gameState.board);
