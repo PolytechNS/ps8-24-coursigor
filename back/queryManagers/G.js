a0a899f0e7715f480dea46ef55f597ffdd5f0075
Swannben <swannbenz@gmail.com>
2024-03-12 13:36:38 +0100

    IA final version

--- back/queryManagers/G.js
+++ back/queryManagers/G.js
@@ -1,17 +1,33 @@
-import { Move, gameState } from './Classes.js';
 
+
+
+class Move {
+    constructor(action, value) {
+      this.action = action;
+      this.value = value;
+    }
+  }
+
+class gameState {
+    constructor(opponentWalls, ownWalls, board) {
+      this.opponentWalls = opponentWalls;
+      this.ownWalls = ownWalls;
+      this.board = board;
+    }
+  };
 const WALL_RIGHT =  0b10000000;
 const WALL_BOTTOM = 0b1000000000;
 const WALL_LEFT =   0b100000000000;
 const WALL_TOP =    0b10000000000000;
 const PLAYER2 =0b10000
 const PLAYER1 =0b100000
-var whichTurn = 0;
+let whichTurn = 1;
 var canSeeOpponent = false;
 var ownPosition = [0,0];
 var opponentPosition = [-1,-1];
 var wallsPlaced = 0;
 var goingLeft = true;
+
 
 var whichPlayer = 0;
 let visionBoard = [ [0,0,0,0,0,0,0,0,0],
@@ -32,12 +48,16 @@ function setup(AIplay) {
         if (AIplay == 1){
             whichPlayer = 1;
             advance = 1;
-            resolve( "20");
+            ownPosition[0] = 3;
+            ownPosition[1] = 1;
+            resolve( "31");
         }
         else {
             whichPlayer = 2;
             advance = -1;
-            resolve("28");
+            ownPosition[0] = 3;
+            ownPosition[1] = 9;
+            resolve("99");
         }
         setTimeout(() => {
             reject('timeout');
@@ -46,13 +66,107 @@ function setup(AIplay) {
 }
 
 function nextMove(gameState) {
-    return new Promise ((resolve, reject) => {
-    resolve(whatToDo(whichPlayer, gameState));
+    return new Promise((resolve, reject) => {
+        let j, i;
+        if (whichTurn<4){
+            if (whichTurn == 1) {
+                if (whichPlayer == 1) {
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: ["17", 0] });
+                } else {
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: ["14", 0] });
+                }
+            } else if (whichTurn == 2 && !canSeeOpponent) {
+                if (whichPlayer == 1) {
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: ["47", 0] });
+                } else {
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: ["44", 0] });
+                }
+            } else if (whichTurn == 3 && !canSeeOpponent) {
+                if (whichPlayer == 1) {
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: ["77", 0] });
+                } else {
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: ["74", 0] });
+                }
+            }
+        }
+        if (canSeeOpponent && wallsPlaced < 10 && whichTurn % 2 == 0 && opponentPosition[0] != ownPosition[0]) {
+            i = opponentPosition[0] -1;
+            j = opponentPosition[1] -1;
+            if (whichPlayer == 2){
+                if (!((visionBoard[i][j] & WALL_TOP) == 0) && !((visionBoard[i+1][j]& WALL_TOP) == 0)){
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: [opponentPosition[0].toString() + opponentPosition[1].toString(), 0] });
+                }else if (((visionBoard[i+1][j] & WALL_TOP) == 0) ){
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: [opponentPosition[0].toString() + opponentPosition[1].toString(), 1] });
+                }else if (((visionBoard[i-1][j]& WALL_TOP) == 0)){
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: [(opponentPosition[0]+1).toString() + opponentPosition[1].toString(), 1] });
+                }
+            }else {
+                if (!((visionBoard[i][j] & WALL_BOTTOM) == 0) && !((visionBoard[i-1][j]& WALL_BOTTOM) == 0)){
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: [opponentPosition[0].toString() + opponentPosition[1].toString(), 0] });
+                }else if (((visionBoard[i-1][j] & WALL_BOTTOM) == 0) ){
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: [opponentPosition[0].toString() + opponentPosition[1].toString(), 1] });
+                }else if (((visionBoard[i+1][j]& WALL_BOTTOM == 0))){
+                    wallsPlaced++;
+                    resolve({ action: "wall", value: [opponentPosition[0].toString() + opponentPosition[1].toString(), 1] });
+                }
+            }
+        }
+
+        i = ownPosition[0]-1;
+        j = ownPosition[1] -1;
+        if (whichPlayer == 1) {
+            if ((visionBoard[i][j] & WALL_TOP) == 0) {
+                resolve({ action: "move", value: ownPosition[0].toString() + (ownPosition[1] + advance).toString() });
+            } else if (goingLeft) {
+                if ((visionBoard[i][j] & WALL_LEFT) == 0 && i<8) {
+                    resolve({ action: "move", value: (ownPosition[0] + 1).toString() + ownPosition[1].toString() });
+                } else if (i>0){
+                    goingLeft = !goingLeft;
+                    resolve({ action: "move", value: (ownPosition[0] - 1).toString() + (ownPosition[1]).toString() });
+                }
+            } else 
+            if ((visionBoard[i][j] & WALL_RIGHT) == 0 && i>0) {
+                resolve({ action: "move", value: (ownPosition[0] - 1).toString() + ownPosition[1].toString() });
+            } else if (j>0) {
+                goingLeft = !goingLeft;
+                resolve({ action: "move", value: ownPosition[0].toString() + (ownPosition[1] - advance).toString() });
+            }
+        } else {
+            if ((visionBoard[i][j] & WALL_BOTTOM) == 0) {
+                resolve({ action: "move", value: ownPosition[0].toString() + (ownPosition[1] + advance).toString() });
+            } else if (goingLeft) {
+                if ((visionBoard[i][j] & WALL_LEFT) == 0 && i<8) {
+                    resolve({ action: "move", value: (ownPosition[0] + 1).toString() + ownPosition[1].toString() });
+                } else {
+                    goingLeft = !goingLeft;
+                    resolve({ action: "move", value: (ownPosition[0] - 1).toString() + (ownPosition[1]).toString() });
+                }
+            } else {
+                if ((visionBoard[i][j] & WALL_RIGHT == 0) && i>0) {
+                    resolve({ action: "move", value: (ownPosition[0] - 1).toString() + ownPosition[1].toString() });
+                } else if (j<8){
+                    goingLeft = !goingLeft;
+                    resolve({ action: "move", value: ownPosition[0].toString() + (ownPosition[1] - advance).toString() });
+                }
+            }
+        }
+        resolve({ action: "move", value: ownPosition[0].toString() + (ownPosition[1] + advance).toString() });
 
     setTimeout(() => {
 
-        resolve(new Move("move",ownPosition[0].toString() + (ownPosition[1]+advance).toString()));
-    }, 200);
+        resolve({ action: "move", value: ownPosition[0].toString() + (ownPosition[1]+advance).toString()});
+    }, 199);
     });
 }
 
@@ -71,13 +185,13 @@ function updateBoard(gameState) {
             for (let j = 0; j < 9; j++){
                 if (gameState.board[i][j] == 1){
                     visionBoard[i][j] = 1;
-                    ownPosition[0] = i;
-                    ownPosition[1] = j;
+                    ownPosition[0] = i+1;
+                    ownPosition[1] = j+1;
                 }
                 else if (gameState.board[i][j] == 2){
                     visionBoard[i][j] = 2;
-                    opponentPosition[0] = i;
-                    opponentPosition[1] = j;
+                    opponentPosition[0] = i+1;
+                    opponentPosition[1] = j+1;
                     canSeeOpponent = true;
                 }
                 else if (gameState.board[i][j] == -1){
@@ -85,45 +199,48 @@ function updateBoard(gameState) {
                 }
             }
         }
-        gameState.opponentWalls.array.forEach(element => {
+
+        gameState.opponentWalls.forEach(element => {
             const element0 = element[0];
             const firstHalf = element0.substring(0, 1);
             const secondHalf = element0.substring(1);
-            firstHalf = parseInt(firstHalf);
-            secondHalf = parseInt(secondHalf);
+            const firstHalfParsed = parseInt(firstHalf) - 1;
+            const secondHalfParsed = parseInt(secondHalf) -1 ;
             if (element[1] == 1){
 
-                visionBoard[firstHalf][secondHalf] = visionBoard[firstHalf][secondHalf] | WALL_RIGHT;
-                visionBoard[firstHalf][secondHalf+1] = visionBoard[firstHalf][secondHalf+1] | WALL_LEFT;
-                visionBoard[firstHalf+1][secondHalf] = visionBoard[firstHalf+1][secondHalf] | WALL_RIGHT;
-                visionBoard[firstHalf+1][secondHalf+1] = visionBoard[firstHalf+1][secondHalf+1] | WALL_LEFT;
+                visionBoard[firstHalfParsed][secondHalfParsed] = visionBoard[firstHalfParsed][secondHalfParsed] | WALL_RIGHT;
+                visionBoard[firstHalfParsed][secondHalfParsed+1] = visionBoard[firstHalfParsed][secondHalfParsed+1] | WALL_LEFT;
+                visionBoard[firstHalfParsed+1][secondHalfParsed] = visionBoard[firstHalfParsed+1][secondHalfParsed] | WALL_RIGHT;
+                visionBoard[firstHalfParsed+1][secondHalfParsed+1] = visionBoard[firstHalfParsed+1][secondHalfParsed+1] | WALL_LEFT;
                 
             }else{
-                visionBoard[firstHalf][secondHalf] = visionBoard[firstHalf][secondHalf] | WALL_BOTTOM;
-                visionBoard[firstHalf+1][secondHalf] = visionBoard[firstHalf+1][secondHalf] | WALL_TOP;
-                visionBoard[firstHalf][secondHalf+1] = visionBoard[firstHalf][secondHalf+1] | WALL_BOTTOM;
-                visionBoard[firstHalf+1][secondHalf+1] = visionBoard[firstHalf+1][secondHalf+1] | WALL_TOP;}
+                visionBoard[firstHalfParsed][secondHalfParsed] = visionBoard[firstHalfParsed][secondHalfParsed] | WALL_BOTTOM;
+                visionBoard[firstHalfParsed+1][secondHalfParsed] = visionBoard[firstHalfParsed+1][secondHalfParsed] | WALL_TOP;
+                visionBoard[firstHalfParsed][secondHalfParsed+1] = visionBoard[firstHalfParsed][secondHalfParsed+1] | WALL_BOTTOM;
+                visionBoard[firstHalfParsed+1][secondHalfParsed+1] = visionBoard[firstHalfParsed+1][secondHalfParsed+1] | WALL_TOP;}
 
         });
-        gameState.ownWalls.array.forEach(element => {
+        gameState.ownWalls.forEach(element => {
+            
             const element0 = element[0];
             const firstHalf = element0.substring(0, 1);
             const secondHalf = element0.substring(1);
-            firstHalf = parseInt(firstHalf);
-            secondHalf = parseInt(secondHalf);
+            
+            const firstHalfParsed = parseInt(firstHalf) -1;
+            const secondHalfParsed = parseInt(secondHalf) -1;
             if (element[1] == 1){
 
-                visionBoard[firstHalf][secondHalf] = visionBoard[firstHalf][secondHalf] | WALL_RIGHT;
-                visionBoard[firstHalf][secondHalf+1] = visionBoard[firstHalf][secondHalf+1] | WALL_LEFT;
-                visionBoard[firstHalf+1][secondHalf] = visionBoard[firstHalf+1][secondHalf] | WALL_RIGHT;
-                visionBoard[firstHalf+1][secondHalf+1] = visionBoard[firstHalf+1][secondHalf+1] | WALL_LEFT;
+                visionBoard[firstHalfParsed][secondHalfParsed] = visionBoard[firstHalfParsed][secondHalfParsed] | WALL_RIGHT;
+                visionBoard[firstHalfParsed][secondHalfParsed+1] = visionBoard[firstHalfParsed][secondHalfParsed+1] | WALL_LEFT;
+                visionBoard[firstHalfParsed+1][secondHalfParsed] = visionBoard[firstHalfParsed+1][secondHalfParsed] | WALL_RIGHT;
+                visionBoard[firstHalfParsed+1][secondHalfParsed+1] = visionBoard[firstHalfParsed+1][secondHalfParsed+1] | WALL_LEFT;
                 
             }else{
-                visionBoard[firstHalf][secondHalf] = visionBoard[firstHalf][secondHalf] | WALL_BOTTOM;
-                visionBoard[firstHalf+1][secondHalf] = visionBoard[firstHalf+1][secondHalf] | WALL_TOP;
-                visionBoard[firstHalf][secondHalf+1] = visionBoard[firstHalf][secondHalf+1] | WALL_BOTTOM;
-                visionBoard[firstHalf+1][secondHalf+1] = visionBoard[firstHalf+1][secondHalf+1] | WALL_TOP;}
-        });
+                visionBoard[firstHalfParsed][secondHalfParsed] = visionBoard[firstHalfParsed][secondHalfParsed] | WALL_BOTTOM;
+                visionBoard[firstHalfParsed+1][secondHalfParsed] = visionBoard[firstHalfParsed+1][secondHalfParsed] | WALL_TOP;
+                visionBoard[firstHalfParsed][secondHalfParsed+1] = visionBoard[firstHalfParsed][secondHalfParsed+1] | WALL_BOTTOM;
+                visionBoard[firstHalfParsed+1][secondHalfParsed+1] = visionBoard[firstHalfParsed+1][secondHalfParsed+1] | WALL_TOP;}
+        });         
         whichTurn++;
         resolve(true);
         setTimeout(() => {resolve(true);}, 50);
@@ -132,109 +249,7 @@ function updateBoard(gameState) {
 
 
 function whatToDo(whichPlayer, gameState){
-    let j,i;
-    if (whichTurn == 1){
-        if (whichPlayer == 1){
-            wallsPlaced++;
-            return new Move("wall", ["60",0]);
-        }
-        else {
-            wallsPlaced++;
-            return new Move("wall", ["20",0]);
-        }
-    }else if (whichTurn == 2 && !canSeeOpponent)
-    {
-        if (whichPlayer == 1){
-            wallsPlaced++;
-            return new Move("wall", ["63",0]);
-        }
-        else {
-            wallsPlaced++;
-            return new Move("wall", ["23",0]);
-        }
-    }else if (whichTurn == 3 && !canSeeOpponent){
-        if (whichPlayer == 1){
-            wallsPlaced++;
-            return new Move("wall", ["66",0]);
-        }
-        else {
-            wallsPlaced++;
-            return new Move("wall", ["26",0]);
-        }
-    }
-    if (canSeeOpponent && wallsPlaced<10 && whichTurn%2 == 0 && opponentPosition[0]!=ownPosition[0]){
-        i = opponentPosition[0];
-        j = opponentPosition[1];
-        if (whichPlayer == 2){
-            if (!(visionBoard[i][j] & WALL_TOP == 0) && !(visionBoard[i+1][j]& WALL_TOP == 0)){
-                wallsPlaced++;
-                return new Move("wall", [i.toString() + j.toString(), 0]);
-            }else if ((visionBoard[i+1][j] & WALL_TOP == 0) ){
-                wallsPlaced++;
-                return new Move("wall", [i.toString() + j.toString(), 1]);
-            }else if ((visionBoard[i-1][j]& WALL_TOP == 0)){
-                wallsPlaced++;
-                return new Move("wall", [i.toString()+1 + j.toString(), 1]);
-            }
-        }else {
-            if (!(visionBoard[i][j] & WALL_BOTTOM == 0) && !(visionBoard[i-1][j]& WALL_BOTTOM == 0)){
-                wallsPlaced++;
-                return new Move("wall", [i.toString() + j.toString(), 0]);
-            }else if ((visionBoard[i-1][j] & WALL_BOTTOM == 0) ){
-                wallsPlaced++;
-                return new Move("wall", [i.toString() + j.toString(), 1]);
-            }else if ((visionBoard[i+1][j]& WALL_BOTTOM == 0)){
-                wallsPlaced++;
-                return new Move("wall", [i.toString()+1 + j.toString(), 1]);
-            }
-
-        }
-    }
-    i = ownPosition[0];
-    j = ownPosition[1];
-    if (gameState.board[i][j] == 1){
-            if (whichPlayer == 1){
-                if (visionBoard[i][j] & WALL_TOP == 0  )
-                    return new Move("move", i.toString() + (j+advance).toString());
-                else if (goingLeft){
-                    if (visionBoard[i][j] & WALL_LEFT == 0){
-                        return new Move("move", (i+1).toString() + j.toString());
-                    }else{
-                        goingLeft = !goingLeft;
-                        return new Move("move", (i-1).toString() + (j).toString());
-                    }
-                
-                }else{
-                    if (visionBoard[i][j] & WALL_RIGHT == 0){
-                        return new Move("move", (i-1).toString() + j.toString());
-                    }else{
-                        goingLeft = !goingLeft;
-                        return new Move("move", i.toString() + (j-advance).toString());
-                    }
-                }
-            }else {
-                if (visionBoard[i][j] & WALL_BOTTOM == 0)
-                    return new Move("move", i.toString() + (j+advance).toString());
-                else if (goingLeft){
-                    if (visionBoard[i][j] & WALL_LEFT == 0){
-                        return new Move("move", (i+1).toString() + j.toString());
-                    }else{
-                        goingLeft = !goingLeft;
-                        return new Move("move", (i-1).toString() + (j).toString());
-                    }
-                }else{
-                    if (visionBoard[i][j] & WALL_RIGHT == 0){
-                        return new Move("move", (i-1).toString() + j.toString());
-                    }else{
-                        goingLeft = !goingLeft;
-                        return new Move("move", i.toString() + (j-advance).toString());
-                    }
-                }
-            }
-    }
-
 }
-
 
     
 
