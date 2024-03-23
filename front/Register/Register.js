299581797ae63773e07fd32b43dc01b4a56452b6
FromentLorenzo <lorenzo.froment@gmail.com>
2024-03-23 23:27:28 +0100

    a bit of logic on login and disconnect display

--- front/Register/Register.js
+++ front/Register/Register.js
@@ -77,7 +77,7 @@ document.getElementById('LoginBtn').addEventListener('click', async function() {
                     const expirationDate = new Date();
                     expirationDate.setTime(expirationDate.getTime() + 60 * 60 * 1000); //expiration 1h
                     document.cookie = `token=${data.token}; expires=${expirationDate.toUTCString()}; path=/`;
-
+                    window.location.href = "../index.html";
                 }
                 console.log('Connexion réussie:', data.message);
             }
